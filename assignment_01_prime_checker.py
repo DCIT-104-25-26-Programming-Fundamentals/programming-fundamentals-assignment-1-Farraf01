7609d241211a26e2e82c99e9287722c767c65ac9
Farraf01 <rfansah@st.ug.edu.gh>
2026-07-27 02:12:39 +0000

    First done

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -35,3 +35,21 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+
+def this_is_prime(number):
+    if number < 2:
+        return False
+
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+
+    return True
+
+
+number = int(input("Enter a number: "))
+
+if this_is_prime(number):
+    print(f"{number} is a prime number")
+else:
+    print(f"{number} is not a prime number")
