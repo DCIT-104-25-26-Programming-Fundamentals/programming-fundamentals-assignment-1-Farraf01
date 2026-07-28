23de3f302817fa1335b80c5a312f8f6ed58adffe
Farraf01 <rfansah@st.ug.edu.gh>
2026-07-28 14:43:30 +0000

    fifth done

--- assignment_05_fibonacci_sequence.py
+++ assignment_05_fibonacci_sequence.py
@@ -1,4 +1,4 @@
-# =============================================================================
+2# =============================================================================
 # PROGRAMMING FUNDAMENTALS — Assignment 5
 # Topic: Loops, Sequences, and Functions
 # =============================================================================
@@ -49,3 +49,47 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+
+def print_fibonacci(n):
+    if n <= 0:
+        print("Error: N must be a positive integer.")
+        return
+
+    first = 0
+    second = 1
+
+    print("Fibonacci sequence:",end=" "  )
+
+    for i in range(n):
+        print(first, end=" ")
+        next_num = first + second
+        first = second
+        second = next_num
+
+    print()  
+
+
+def check_fibonacci(number):
+    if number < 0:
+        print(f"{number} is NOT a Fibonacci number.")
+        return
+
+    first = 0
+    second = 1
+
+    while first < number:
+        next_num = first + second
+        first = second
+        second = next_num
+
+    if first == number:
+        print(f"{number} is a Fibonacci number.")
+    else:
+        print(f"{number} is NOT a Fibonacci number.")
+
+
+n = int(input("How many terms? "))
+print_fibonacci(n)
+
+num = int(input("Enter a number to check: "))
+check_fibonacci(num)
